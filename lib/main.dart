401211c9d222ed4e95cbe1823a980c00bd757419
Milosz Ortmann <greentx@wp.pl>
2023-02-09 13:55:46 +0100

    login page update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
@@ -43,11 +44,80 @@ class _MyHomePageState extends State<MyHomePage> {
 
       body: Center(
 
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: const [
-              Text('Welcome',
-              )
+        child:  Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+             Row(
+               mainAxisAlignment: MainAxisAlignment.center,
+               children: [
+                 Padding(padding: EdgeInsets.only(top: 150),),
+                 Text('Welcome',
+                   style: TextStyle(
+                     fontSize: 40,
+                   ),
+                 ),
+                 
+               ],
+             ),
+              Stack(
+                children: [
+                  Container(
+                    height: 230,
+                    width: 300,
+
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: 'Enter your e-mail',
+                          ),
+                        ),
+                        Padding(padding: EdgeInsets.symmetric(vertical: 30),),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: 'Enter your password',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                ],
+
+              ),
+              SizedBox(
+                width: 300,
+                height: 50,
+                child:
+                ElevatedButton(onPressed: (){
+                  //code
+                },
+                  child: Text('Login',
+                    style: TextStyle(
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                width: 300,
+                height: 50,
+
+                child:
+                TextButton(onPressed: (){
+                  //code
+                },
+                  child: Text('Register Account',
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ),
+
           ],
         ),
       ),
