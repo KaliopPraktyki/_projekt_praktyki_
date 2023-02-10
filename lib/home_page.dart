0111b846ce64391cddf8e23023150d1cc2682bef
Milosz Ortmann <greentx@wp.pl>
2023-02-10 11:41:50 +0100

    login done

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,10 +9,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final user = FirebaseAuth.instance.currentUser!;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('data'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('singed in as: ' + user.email!),
+            TextButton(onPressed: (){
+              FirebaseAuth.instance.signOut();
+            },
+                child: Text('Log out'))
+          ],
+        ),
+      ),
     );
   }
   
