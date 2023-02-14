7f12e86159d7ff5e057963dbda101377c87c0bdc
Krystian <kryroz21@gmail.com>
2023-02-14 11:09:33 +0100

    todo fixed

--- lib/auth/main_screen.dart
+++ lib/auth/main_screen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:loginscreen/auth/auth_page.dart';
 import 'package:loginscreen/screens/profilescreen.dart';
+import 'package:loginscreen/screens/todo_page.dart';
 import '../screens/home_page.dart';
 
 class MainPage extends StatelessWidget {
@@ -14,7 +15,7 @@ class MainPage extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot){
           if (snapshot.hasData) {
-            return ProfileScreen();
+            return to_do();
           }else {
             return AuthPage();
           }
