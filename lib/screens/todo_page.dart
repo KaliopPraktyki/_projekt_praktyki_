cb81a7abf805813999171a70047e4d4164e0e64f
Krystian <kryroz21@gmail.com>
2023-02-14 11:07:36 +0100

    todo fixed

--- lib/screens/todo_page.dart
+++ lib/screens/todo_page.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:ionicons/ionicons.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-        brightness: Brightness.light,
-        primaryColor: Colors.blue,
-    ),
-    home: MyApp(),
-  ));
-}
 
-class MyApp extends StatefulWidget {
+class to_do extends StatefulWidget {
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<to_do> createState() => _to_doState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _to_doState extends State<to_do> {
   //List todo = List();
 
-  //final List<String> items = List<String>.generate(10000, (i) => '$i');
   String input = "";
 
   var todo = ["Uśmiechnij się!"];
+
+  // createTodo() {
+  //   DocumentReference documentReference =
+  //     Firestore.instance.collection("All ToDos").document(input);
+  //
+  // }
+
+  // deletetodo() {
+  //
+  // }
 
   // @override
   // void initState() {
@@ -36,20 +36,15 @@ class _MyAppState extends State<MyApp> {
   //   todo.add("item4");
   // }
 
-  // icon: Icon(
-  // Icons.delete,
-  // color: Colors.red
-  // ),
-
-  //Text("ToDos")
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(""),
+        title: Text("All ToDos",
+          style: TextStyle(color: Colors.black),),
         backgroundColor: Colors.white,
         shadowColor: Colors.white,
+        elevation: 0,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
@@ -88,53 +83,29 @@ class _MyAppState extends State<MyApp> {
           itemBuilder: (BuildContext context, int index) {
             return Dismissible(
                 key: Key(todo[index]),
-                child: Card(
-                  elevation: 2,
-                  margin: EdgeInsets.all(8),
-                  shape: RoundedRectangleBorder(borderRadius:
-                  BorderRadius.circular(8)),
-                  child: ListTile(
-                    title: Text(todo[index]),
-                    trailing: IconButton(
-                        icon: Icon(
-                            Icons.delete,
-                            color: Colors.red
-                        ),
-                        onPressed: (){
-                          setState(() {
-                            todo.removeAt(index);
-                          });
-                        }),
-                  ),
-                ));
-          }),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 7.0, vertical: 10),
 
-        child: GNav(
-          tabBackgroundColor: Colors.grey[300]!,
-          gap: 8,
-          padding: EdgeInsets.all(16),
-          tabs: const [
-            GButton(icon:
-            Ionicons.home_outline,
-              text: 'Home',
-            ),
-            GButton(icon:
-            Ionicons.calendar_outline,
-              text: 'Sheadule',
-            ),
-            GButton(icon:
-            Ionicons.list_outline,
-              text: 'Todos',
-            ),
-            GButton(icon:
-            Ionicons.person_outline,
-              text: 'Profile',
-            ),
-          ],
-        ),
-      ),
+
+              child: Card(
+                elevation: 2,
+                margin: EdgeInsets.all(8),
+                shape: RoundedRectangleBorder(borderRadius:
+                BorderRadius.circular(8)),
+                child: ListTile(
+                  title: Text(todo[index]),
+                  trailing: IconButton(
+                      icon: Icon(
+                          Icons.delete,
+                          color: Colors.red
+                      ),
+                      onPressed: (){
+                        setState(() {
+                          todo.removeAt(index);
+                        });
+                      }),
+                ),
+              ),
+            );
+          }),
     );
   }
 }
