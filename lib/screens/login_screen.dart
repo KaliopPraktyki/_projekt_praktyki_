80693ca3bf26873693d60ef6894d5cea0150749a
Milosz Ortmann <greentx@wp.pl>
2023-02-16 09:48:02 +0100

    simple validatior added to login screen update register page

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:validators/validators.dart';
 
 class loginscreen extends StatefulWidget {
   final VoidCallback showRegisterPage;
@@ -11,29 +12,35 @@ class loginscreen extends StatefulWidget {
 
 class _loginscreenState extends State<loginscreen> {
 
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
+    final _emailController = TextEditingController();
+    final _passwordController = TextEditingController();
 
-  Future signIn() async {
-    await FirebaseAuth.instance
-        .signInWithEmailAndPassword(
-        email: _emailController.text.trim(),
-        password: _passwordController.text.trim(),
-    );
-  }
+    Future signIn() async {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+          email: _emailController.text.trim(),
+          password: _passwordController.text.trim(),
+      );
+    }
 
-  @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    super.dispose();
-  }
+    @override
+    void dispose() {
+      _emailController.dispose();
+      _passwordController.dispose();
+      super.dispose();
+    }
 
-  late bool _passwordVisible;
+    late bool _passwordVisible;
 
-  void initState() {
-    _passwordVisible = false;
-  }
+    void initState() {
+      _passwordVisible = false;
+    }
+
+    String _password = '';
+
+    final _formKey = GlobalKey<FormState>();
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -41,124 +48,150 @@ class _loginscreenState extends State<loginscreen> {
 
       body: Center(
 
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(padding: EdgeInsets.only(top: 150),),
-                Text('Welcome',
-                  style: TextStyle(
-                    fontFamily: 'Spoof',
-                    fontSize: 40,
-                  ),
-                ),
+        child: Form(
 
-              ],
-            ),
-            Stack(
-              children: [
-                Container(
-                  height: 230,
-                  width: 300,
-
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        controller: _emailController,
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'Enter your e-mail',
-                        ),
-                      ),
-                      Padding(padding: EdgeInsets.symmetric(vertical: 20),),
-                      TextFormField(
-                        keyboardType: TextInputType.text,
-                        controller: _passwordController,
-                        obscureText: !_passwordVisible,//This will obscure text dynamically
-                        decoration: InputDecoration(
-                          labelText: 'Password',
-                          hintText: 'Enter your password',
-                          // Here is key idea
-                          suffixIcon: IconButton(
-                            icon: Icon(
-                              // Based on passwordVisible state choose the icon
-                              _passwordVisible
-                                  ? Icons.visibility
-                                  : Icons.visibility_off,
-                              color: Theme.of(context).primaryColorDark,
-                            ),
-                            onPressed: () {
-                              // Update the state i.e. toogle the state of passwordVisible variable
-                              setState(() {
-                                _passwordVisible = !_passwordVisible;
-                              });
-                            },
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-
-              ],
-
-            ),
-            SizedBox(
-              width: 300,
-              height: 50,
-              child:
-              GestureDetector(
-                onTap: signIn,
-                child: ElevatedButton(onPressed: () {
-                  signIn();
-                },
-                  child: Text('Login',
+          child:  Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(padding: EdgeInsets.only(top: 150),),
+                  Text('Welcome',
                     style: TextStyle(
-                      fontSize: 25,
+                      fontFamily: 'Spoof',
+                      fontSize: 40,
+                    ),
+                  ),
+
+                ],
+              ),
+              Stack(
+                children: [
+                  Container(
+                    height: 230,
+                    width: 300,
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            controller: _emailController,
+                            keyboardType: TextInputType.emailAddress,
+                            validator: (value) {
+                              if (value != null && value.isEmpty) {
+                                return 'Please enter email adress';
+                              }
+                              return null;
+                            },
+                            decoration: const InputDecoration(
+                              border: UnderlineInputBorder(),
+                              labelText: 'Enter your e-mail',
+                            ),
+                          ),
+                          Padding(padding: EdgeInsets.symmetric(vertical: 20),),
+                          TextFormField(
+                            keyboardType: TextInputType.text,
+                            controller: _passwordController,
+                            obscureText: !_passwordVisible,//This will obscure text dynamically
+                            onChanged: (value){
+                              _password = value;
+                            },
+                            validator: (value) {
+                              if (value != null && value.isEmpty) {
+                                return 'Password is required please enter';
+                              } if (_password.length <= 6) {
+                                return 'Min. lenght password is 6';
+                              }
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              hintText: 'Enter your password',
+                              // Here is key idea
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  // Based on passwordVisible state choose the icon
+                                  _passwordVisible
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                  color: Theme.of(context).primaryColorDark,
+                                ),
+                                onPressed: () {
+                                  // Update the state i.e. toogle the state of passwordVisible variable
+                                  setState(() {
+                                    _passwordVisible = !_passwordVisible;
+                                  });
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+
+                ],
+
+              ),
+              SizedBox(
+                width: 300,
+                height: 50,
+                child:
+                GestureDetector(
+                  onTap: signIn,
+                  child: ElevatedButton(onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      signIn();
+                  }
+                  },
+                    child: Text('Login',
+                      style: TextStyle(
+                        fontSize: 25,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
 
-            SizedBox(
-              width: 280,
-              height: 50,
+              SizedBox(
+                width: 280,
+                height: 50,
 
-              child:
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
+                child:
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
 
-                  Row(
-                    children: [
-                      Padding(padding: EdgeInsets.only(left: 30),),
-                      Text('Not a member?'),
+                    Row(
+                      children: [
+                        Padding(padding: EdgeInsets.only(left: 30),),
+                        Text('Not a member?'),
 
-                      TextButton(onPressed: () {
-                        //code
-                      },
-                        child: GestureDetector(
-                          onTap: widget.showRegisterPage,
-                          child: Text('Register now.',
-                            style: TextStyle(
-                              fontSize: 15,
+                        TextButton(onPressed: () {
+                          //code
+                        },
+                          child: GestureDetector(
+                            onTap: widget.showRegisterPage,
+                            child: Text('Register now.',
+                              style: TextStyle(
+                                fontSize: 15,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
+
               ),
 
-            ),
+            ],
+          ),
+        )
 
-          ],
-        ),
       ),
     );
   }
