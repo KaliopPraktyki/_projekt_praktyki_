33757c7d092280e9b16a9714f7a30d7829c8d893
Milosz Ortmann <greentx@wp.pl>
2023-02-20 09:37:00 +0100

    email verify added

---
+++ lib/auth/verify_email_page.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:loginscreen/screens/home_page.dart';
+
+
+class VerifyEmailPage extends StatefulWidget {
+
+  @override
+  State<VerifyEmailPage > createState() => _VerifyEmailPageState();
+}
+
+class _VerifyEmailPageState extends State<VerifyEmailPage> {
+  bool isEmailVerified = false;
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    isEmailVerified = FirebaseAuth.instance.currentUser!.emailVerified;
+
+    if (!isEmailVerified){
+      sendVerificationEmail();
+
+      timer = Timer.periodic(
+        Duration(seconds: 3),
+          (_) => checkEmailVerified(),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+
+    super.dispose();
+  }
+
+  Future checkEmailVerified() async {
+   await FirebaseAuth.instance.currentUser!.reload();
+
+    setState(() {
+      isEmailVerified = FirebaseAuth.instance.currentUser!.emailVerified;
+    });
+
+    if (isEmailVerified) timer?.cancel();
+  }
+
+  Future sendVerificationEmail() async {
+    try {
+      final user = FirebaseAuth.instance.currentUser!;
+      await user.sendEmailVerification();
+    }catch (e) {
+      print(e.toString());
+    }
+    }
+
+  @override
+  Widget build(BuildContext context) => isEmailVerified
+      ?HomePage()
+      :Scaffold(
+      appBar: AppBar(
+        title: Text('Verify Email'),
+      ),
+  );
+}
