3aa3029dcaf4bb016b2cd36b9c72eb61acd41d2d
Kamil <Juraszek.Kamil@drzewniak.slupsk.pl>
2023-02-21 11:15:31 +0100

    Changed the way to add events, added description tab

--- lib/screens/schedule.dart
+++ lib/screens/schedule.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+//TODO: Firestore configuration
 
 class Schedule extends StatefulWidget {
   @override
@@ -11,42 +13,37 @@ class Schedule extends StatefulWidget {
 
 }
 
-class Event {
-  final String title;
-  Event({required this.title});
-
-  String toString() => this.title;
-}
-
 class _ScheduleState extends State<Schedule> {
   final user = FirebaseAuth.instance.currentUser!;
 
-  late Map<DateTime, List<Event>> _selectedEvents;
-
-  Map<DateTime, List<Event>> get selectedEvents => _selectedEvents;
-
-  set selectedEvents(Map<DateTime, List<Event>> value) {
-    _selectedEvents = value;
-  }
   CalendarFormat format = CalendarFormat.month;
-  DateTime selectedDay = DateTime.now();
-  DateTime focusedDay = DateTime.now();
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDate;
 
-  final TextEditingController _eventController = TextEditingController();
+  Map<String, List> selectedEvents = {};
+
+  final _titleController = TextEditingController();
+  final _descrController = TextEditingController();
 
   @override
   void initState() {
     selectedEvents = {};
+    _selectedDate = _focusedDay;
     super.initState();
   }
 
-  List<Event> _getEventsfromDay(DateTime date) {
-    return selectedEvents[date] ?? [];
+   List _getEventsfromDay(date) {
+    if (selectedEvents[DateFormat('yyyy-MM-dd').format(date)] != null) {
+    return selectedEvents[DateFormat('yyyy-MM-dd').format(date)]!;
+    } else {
+     return selectedEvents[DateTime] ?? [];
+   }
   }
 
   @override
   void dispose() {
-    _eventController.dispose();
+    _titleController.dispose();
+    _descrController.dispose();
     super.dispose();
   }
 
@@ -57,13 +54,13 @@ class _ScheduleState extends State<Schedule> {
           child: Column(
             children: [
               TableCalendar(
-                focusedDay: selectedDay,
+                focusedDay: _focusedDay,
                 firstDay: DateTime(1990),
                 lastDay: DateTime(2050),
                 calendarFormat: format,
-                onFormatChanged: (CalendarFormat _format) {
+                onFormatChanged: (CalendarFormat format) {
                   setState(() {
-                    format = _format;
+                    format = format;
                   });
                 },
                 startingDayOfWeek: StartingDayOfWeek.sunday,
@@ -72,13 +69,13 @@ class _ScheduleState extends State<Schedule> {
                 //Day Changed
                 onDaySelected: (DateTime selectDay, DateTime focusDay) {
                   setState(() {
-                    selectedDay = selectDay;
-                    focusedDay = focusDay;
+                    _selectedDate = selectDay;
+                    _focusedDay = focusDay;
                   });
-                  print(focusedDay);
+                  print(_focusedDay);
                 },
                 selectedDayPredicate: (DateTime date) {
-                  return isSameDay(selectedDay, date);
+                  return isSameDay(_selectedDate, date);
                 },
 
                 eventLoader: _getEventsfromDay,
@@ -87,22 +84,18 @@ class _ScheduleState extends State<Schedule> {
                   isTodayHighlighted: true,
                   selectedDecoration: BoxDecoration(
                     color: Theme.of(context).secondaryHeaderColor,
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.circular(10.0),
+                    shape: BoxShape.circle,
                   ),
                   selectedTextStyle: const TextStyle(color: Colors.white),
                   todayDecoration: BoxDecoration(
                     color: Colors.blue,
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.circular(10.0),
+                    shape: BoxShape.circle,
                   ),
                   defaultDecoration: BoxDecoration(
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.circular(10.0),
+                    shape: BoxShape.circle,
                   ),
                   weekendDecoration: BoxDecoration(
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.circular(10.0),
+                    shape: BoxShape.circle,
                   ),
                 ),
                 headerStyle: HeaderStyle(
@@ -120,13 +113,18 @@ class _ScheduleState extends State<Schedule> {
               ),
               Text(
                 AppLocalizations.of(context)!.events,
-                  style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+                  style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
               ),
-              ..._getEventsfromDay(selectedDay).map(
-                    (Event event) => ListTile(
-                  title: Text(
-                    event.title,
-                  ),
+              ..._getEventsfromDay(_focusedDay).map(
+                    (event) => ListTile(
+                      leading: const Icon(
+                        Icons.circle,
+                      ),
+                      title: Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: Text('Title:   ${event['eventTitle']}'),
+                      ),
+                      subtitle: Text('Description:   ${event['eventDescr']}'),
                 ),
               ),
             ],
@@ -139,8 +137,21 @@ class _ScheduleState extends State<Schedule> {
         title: Text(
           AppLocalizations.of(context)!.addevent,
         ),
-        content: TextFormField(
-          controller: _eventController,
+        content:Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+         TextField(
+          controller: _titleController,
+          decoration: const InputDecoration(
+          labelText: 'Title',
+        ),),
+        TextField(
+          controller: _descrController,
+          decoration: const InputDecoration(
+            labelText: 'Description',
+          ),),
+        ],
         ),
         actions: [
           TextButton(
@@ -148,26 +159,48 @@ class _ScheduleState extends State<Schedule> {
             onPressed: () => Navigator.pop(context),
           ),
           TextButton(
-            child: const Text("Ok"),
+            child: const Text("Add"),
             onPressed: () {
-              if (_eventController.text.isEmpty) {
+              if (_titleController.text.isEmpty &&
+                  _descrController.text.isEmpty) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Required title and description'),
+                    duration: Duration(seconds: 2),
+                  ),
+                );
+                //Navigator.pop(context);
+                return;
               } else {
-                if (selectedEvents[selectedDay] != null) {
-                  selectedEvents[selectedDay]?.add(
-                    Event(title: _eventController.text),
-                  );
-                } else {
-                  selectedEvents[selectedDay] = [
-                    Event(title: _eventController.text)
-                  ];
-                }
 
+                print(_titleController.text);
+                print(_descrController.text);
+
+                setState(() {
+                  if (selectedEvents[
+                  DateFormat('yyyy-MM-dd').format(_selectedDate!)] !=
+                      null) {
+                    selectedEvents[
+                    DateFormat('yyyy-MM-dd').format(_selectedDate!)]
+                        ?.add({
+                      "eventTitle": _titleController.text,
+                      "eventDescr": _descrController.text,
+                    });
+                  } else {
+                    selectedEvents[
+                    DateFormat('yyyy-MM-dd').format(_selectedDate!)] = [
+                      {
+                        "eventTitle": _titleController.text,
+                        "eventDescr": _descrController.text,
+                      }
+                    ];
+                  }
+                });
+                Navigator.pop(context);
+                setState(() {});
+                return;
               }
-              Navigator.pop(context);
-              _eventController.clear();
-              setState((){});
-              return;
-            },
+            }
           ),
         ],
       ),
