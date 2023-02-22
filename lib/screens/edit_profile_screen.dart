ff8304298a7240c47e346155badf8b617e352da7
Mykola <mykola.terianyk@drzewniak.slupsk.pl>
2023-02-22 13:52:32 +0100

    Edit profile screen ui

--- lib/screens/edit_profile_screen.dart
+++ lib/screens/edit_profile_screen.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:loginscreen/screens/settings_screen.dart';
+import 'dart:io';
 
 class EditProfile extends StatefulWidget {
   const EditProfile({Key? key}) : super(key: key);
@@ -14,13 +16,34 @@ class EditProfile extends StatefulWidget {
 
 class _EditProfileState extends State<EditProfile> {
 
+  String? _firstName;
+  String? _lastName;
+  String? _birthday;
+  File? _profileImage;
   TextEditingController dateInput = TextEditingController();
+  displayProfileImage(){
+    if(_profileImage == null){
+      return const AssetImage('assets/images/prof.jpg');
+    }else{
+      return FileImage(_profileImage!);
+    }
+  }
+  saveProfile(){
 
+  }
   void initState() {
-    dateInput.text = ""; //set the initial value of text field
+     //set the initial value of text field
+    if(_birthday == null){
+      dateInput.text = "";
+
+    }else{
+      dateInput.text = _birthday!;
+    }
     super.initState();
   }
+
   final  _formKey = GlobalKey<FormState>();
+  bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -42,15 +65,36 @@ class _EditProfileState extends State<EditProfile> {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                  width: 120,
-                  height: 120,
-                  decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(100),
-                  color: Theme.of(context).cardColor,
+              Stack(
+                children: [
+                  CircleAvatar(
+                    radius: 60,
+                    backgroundImage: displayProfileImage(),
                   ),
-                  child: Icon(Ionicons.camera, size: 40,),
-                ),
+                  CircleAvatar(
+                    radius: 60,
+                    backgroundColor: Theme.of(context).cardColor,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Icon(
+                          Ionicons.camera,
+                          size: 45,
+                        ),
+                        Text(
+                            "Change Profile Photo",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ],
           ),
         Row(
@@ -65,22 +109,42 @@ class _EditProfileState extends State<EditProfile> {
                     Padding(
                       padding: const EdgeInsets.only(top: 10, bottom: 10,),
                       child: TextFormField(
-                        initialValue: "User firstName",
+                        initialValue: _firstName,
                         decoration: const InputDecoration(
                           border: UnderlineInputBorder(),
                           labelText: "First name",
                         ),
+                        validator: (input){
+                          if(input == null){
+                            return "Please enter name";
+                          }else if(input.length <2 ){
+                            return "Please enter valid name";
+                          }
+                        },
+                        onSaved: (value){
+                          _firstName = value;
+                        },
                         onChanged: (value){},
                       ),
                     ),
                     Padding(
                       padding: const EdgeInsets.only(top: 10,bottom: 10,),
                       child: TextFormField(
-                        initialValue: "User lastName",
+                        initialValue: _lastName,
                         decoration: const InputDecoration(
                           border: UnderlineInputBorder(),
                           labelText: "Last name",
                         ),
+                        validator: (input){
+                         if(input == null){
+                           return "Please enter last name";
+                         }else if(input.length <2 ){
+                           return "Please enter valid last name";
+                         }
+                        },
+                        onSaved: (value){
+                          _lastName = value;
+                        },
                         onChanged: (value){},
                       ),
                     ),
@@ -116,6 +180,11 @@ class _EditProfileState extends State<EditProfile> {
                         },
                       ),
                     ),
+                    _isLoading?
+                        CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation(Theme.of(context).cardColor),
+                        )
+                        :SizedBox.shrink()
                   ],
                 ),
               ),
@@ -144,6 +213,7 @@ class _EditProfileState extends State<EditProfile> {
                     width: 150,
                     height: 40,
                     child: GestureDetector(
+                      onTap: saveProfile(),
                       child: ElevatedButton(
                         onPressed: (){},
                         child: Text("Save"),
@@ -153,7 +223,6 @@ class _EditProfileState extends State<EditProfile> {
                 ),
               ],
             )
-
           ],
         ),
       ),
